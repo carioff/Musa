574427ab2bd9642981dc6fcc7b8cb440eae3a2b1
carioff <carioff@naver.com>
2017-03-13 17:23:22 +0900

    Musa First
    Musa First

---
+++ Assets/02.Scripts/MusaCtrl.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class MusaCtrl : MonoBehaviour {
+
+    private Transform tr;
+    private Animator anim;
+    private NavMeshAgent nv;
+
+    public float damping = 10.0f;
+    public float speed = 1.0f;
+
+    private Ray ray;
+    private RaycastHit hit;
+    private int floorLayer;
+   
+    public Vector3 movePos = Vector3.zero;
+	// Use this for initialization
+	void Start () {
+        tr = GetComponent<Transform>();
+        anim = GetComponent<Animator>();
+        nv = GetComponent<NavMeshAgent>();
+        floorLayer = LayerMask.NameToLayer("FLOOR");
+
+        movePos = tr.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        if (Input.GetMouseButtonDown(0) && Physics.Raycast(ray, out hit, 100.0f, 1 << floorLayer))
+        {
+            movePos = hit.point;
+            Debug.Log(movePos);
+            nv.SetDestination(movePos);
+            nv.Resume();
+        }
+            
+        if (nv.velocity.magnitude >= 0.1f && nv.remainingDistance >= 0.2f)
+        {
+            anim.SetBool("isRun", true);
+        }
+        else
+        {
+            anim.SetBool("isRun", false);
+        }   
+//        if ((movePos - tr.position).sqrMagnitude >= 0.2f)
+//        {
+//            Quaternion rot = Quaternion.LookRotation(movePos - tr.position);
+//            tr.rotation = Quaternion.Slerp(tr.rotation, rot, Time.deltaTime * damping);
+//            tr.Translate(Vector3.forward * Time.deltaTime * speed);
+//            anim.SetBool("isRun", true);
+//           
+//        }
+//        else
+//        {
+//            anim.SetBool("isRun", false);
+//        }
+	}
+}
---
+++ Assets/02.Scripts/TouchMgr.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TouchMgr : MonoBehaviour {
+
+    private Ray ray;
+    private RaycastHit hit;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        #if UNITY_ANDROID
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            Application.Quit();
+
+        }
+
+        if(Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
+        {
+            ray = Camera.main.ScreenPointToRay(Input.GetTouch(0).position);
+
+            if (Physics.Raycast(ray, out hit, 100.0f, 1 << 8))
+            {
+                Destroy(hit.collider.gameObject);
+                ExpBox();
+            }
+        }
+        #endif
+
+        #if UNITY_EDITOR
+        ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        Debug.DrawRay(ray.origin, ray.direction * 100.0f, Color.green);
+
+        if (Input.GetMouseButtonDown(0) && Physics.Raycast(ray, out hit, 100.0f, 1 << 8))
+        {
+            Destroy(hit.collider.gameObject);
+            ExpBox();
+        }
+        #endif
+	}
+
+    void ExpBox()
+    {
+        //순간적으로 오버랩 
+        Collider[] colls = Physics.OverlapSphere(hit.point, 10.0f, 1 << 9);
+        foreach (var coll in colls)
+        {
+            coll.GetComponent<Rigidbody>().AddExplosionForce(1500.0f, hit.point, 10.0f, 1200.0f);
+        }
+    }
+}
